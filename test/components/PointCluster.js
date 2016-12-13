ff5a6d0dd45cea513e2527641e5740d45a2804c0
JP Given <jgiven@gmail.com>
2016-12-13 11:36:07 -0800

    Getting mocha tests running.

--- test/components/PointCluster.js
+++ test/components/PointCluster.js
@@ -1,11 +1,8 @@
-var _ = require('underscore');
-var chai = require('chai');
-import { PointCluster } from '../../src/components/PointCluster'
 
 describe('#setCollection()', function () {
   it('should be an array of lat/lng objects', function () {
     var pc = new PointCluster({
-      map: ''
+      map: window.map
     });
     pc.setCollection([
       {
--- webpack.base.config.js
+++ webpack.base.config.js
@@ -16,7 +16,7 @@ module.exports = {
       },
       {
         test: /\.scss$/,
-        loaders: ["style", "css", "sass"]
+        loaders: ["style", "css", "sass?sourceMap"]
       },
       {
         test: /\.jsx?$/,
