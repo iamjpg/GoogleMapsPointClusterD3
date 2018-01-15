8720d9454a34c11e7d7d9f4557e832fdde5d0872
JP Given <jgiven@gmail.com>
2018-01-15 12:30:14 -0800

    Working on tests.

--- test/components/PointCluster.js
+++ test/components/PointCluster.js
@@ -1,15 +1,40 @@
+var pc = new PointCluster({
+  map: window.map
+});
+
+pc.setCollection([
+  // SF
+  {
+    "lat": 37.937238,
+    "lng": -121.719173
+  },
+  // DC
+  {
+    "lat": 38.900497,
+    "lng": -77.007507
+  }
+])
+
 
 describe('#setCollection()', function () {
   it('should be an array of lat/lng objects', function () {
-    var pc = new PointCluster({
-      map: window.map
-    });
-    pc.setCollection([
-      {
-        "lat": 37.937238,
-        "lng": -121.719173
-      }
-    ])
     chai.assert.isTrue(_.isArray(pc.collection), 'is an array of objects.');
   });
 });
+
+describe('#createOverlay()', function () {
+  it('overlay should be an object', function () {
+    pc.createOverlay()
+    chai.assert.isObject(pc.overlay, 'is an object');
+  });
+});
+
+describe('#checkIfLatLngInBounds()', function () {
+  it('should have two lat lngs but only return one.', function () {
+    // Timeout because undefined map issues.
+    setTimeout(function () {
+      var arr = pc.checkIfLatLngInBounds()
+      chai.assert.strictEqual(arr.length, 1, 'Two points; only one in bounds - so one returned.');
+    }, 500)
+  });
+})
